63ff4b4885fe85329302c976c46dd9e2cd0ba8dd
k.patil <k.patil@fz-juelich.de>
2021-09-03 18:53:39 +0200

    datalad and qctool interface

--- jusnp.py
+++ jusnp.py
@@ -1,8 +1,11 @@
-import requests
 import io
 import os
+import glob
+import shutil
+import subprocess
+import requests
 import pandas as pd
-
+from datalad import api as datalad
 
 def ensembl_human_rsid(rsid):
     """
@@ -12,6 +15,107 @@ def ensembl_human_rsid(rsid):
     response = requests.get(url)
     return response
 
+
+def datalad_get_chromosome(c,
+        source=None,
+        imputationdir='imputation',
+        path=None):
+    """
+    get a particular chromosome's (imputed) data
+    """
+    if source is None or source == '':
+        source="ria+http://ukb.ds.inm7.de#~genetic"
+
+    if path is None or path == '':
+        path = os.path.join('/tmp', 'genetic')
+
+    ds = datalad.clone(source=source, path=path)
+    files = glob.glob(os.path.join(ds.path, imputationdir, '*_c' + str(c) + '_*'))
+    ds.get(files)
+    return files, ds
+
+
+def rsid2chromosome(rsids):
+    if isinstance(rsids, str) and os.path.isfile(rsids):
+        rsids = pd.read_csv(rsids, header=None)
+        rsids = list(rsids.iloc[:,0])
+    elif isinstance(rsids, str):
+        rsids = [rsids]
+
+    chromosomes = [None] * len(rsids)
+    for rs in range(len(rsids)):
+        ens = ensembl_human_rsid(rsids[rs])
+        ens = ens.json()
+        ens = ens['mappings']
+        for m in range(len(ens)):
+            if ens[m]['ancestral_allele'] is not None:
+                chromosomes[rs] = ens[m]['seq_region_name']
+
+    df = pd.DataFrame()
+    df['chromosomes'] = chromosomes
+    df['rsids'] = rsids
+    return df
+
+
+def rsid2vcf(rsids, outdir,
+        datalad_source="ria+http://ukb.ds.inm7.de#~genetic",
+        qctool=None,
+        datalad_drop=True,
+        tmpdir='/tmp'):
+    # check if qctool is available
+    if qctool is None:
+        qctool = shutil.which('qctool')
+
+    if qctool is None:
+        print('qctool is not available')
+        raise
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    if os.listdir(outdir):
+        print('the output directory must be empty')
+        raise
+
+    # get chromosome of each rsid
+    ch_rs = rsid2chromosome(rsids)
+    uchromosomes = pd.unique(ch_rs['chromosomes'])
+    print('chromosomes needed: ' + str(uchromosomes) + '\n')
+    for c in range(len(uchromosomes)):
+        ch = uchromosomes[c]
+        ind = [i for i, x in enumerate(ch_rs['chromosomes']) if x == uchromosomes[c]]
+        rs_ch = [rsids[i] for i in ind]
+        print('chromosome ' + str(ch) + ' with ' + str(len(rs_ch)) + ' rsids\n')
+        if len(rs_ch) < 11:
+            print('rsids: ' + str(rs_ch) + '\n')
+        # get the data
+        files, ds = datalad_get_chromosome(ch, source=datalad_source)
+        # find the bgen and sample files
+        file_bgen = None
+        file_sample = None
+        for fl in files:
+            name, ext = os.path.splitext(fl)
+            if ext == '.bgen':
+                assert file_bgen is None
+                file_bgen = fl
+            elif ext == '.sample':
+                assert file_sample is None
+                file_sample = fl
+
+        assert file_bgen is not None and file_sample is not None
+        file_rsids = os.path.join(outdir, 'rsids_chromosome' + str(ch) + '.txt')
+        file_vcf = os.path.join(outdir, 'chromosome' + str(ch) + '.vcf')
+        cmd = qctool + ' -g ' + file_bgen + ' -s ' + file_sample \
+              + ' -incl-rsids ' + file_rsids  + ' -og ' + file_vcf
+        printf('running qctool:' + cmd  + '\n')
+        subprocess.run(cmd)
+
+        if datalad_drop:
+            ds.drop(files)
+
+        print('done with chromosome ' + str(ch) + '\n')
+
+    return ch_rs
 
 def read_vcf(path):
     """
@@ -37,7 +141,7 @@ def vcf2genotype(vcf, th=0.9, snps=None, samples=None):
     elif isinstance(vcf, pd.DataFrame):
         pass
     else:
-        print("don't know how to handle vcf")
+        print("don't know how to handle the input")
         raise
 
     format = pd.unique(vcf['FORMAT'])
