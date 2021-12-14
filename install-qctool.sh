707338392c9c93541ae4119bd245c4a54dc4a5c7
Fede Raimondo <federaimondo@gmail.com>
2021-12-14 17:53:02 +0100

    another approach 3

--- install-qctool.sh
+++ install-qctool.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 mkdir tmp
-cd tmp
-git clone https://github.com/gavinband/qctool
-cd qctool
-./waf configure --prefix=/usr/bin/
-./waf
-./waf install
+cd tmp || exit
+# git clone https://github.com/gavinband/qctool.git
+# cd qctool || exit
+# ./waf configure --prefix=/usr/bin/
+# ./waf
+# ./waf install
+wget https://www.well.ox.ac.uk/~gav/resources/qctool_v2.2.0-CentOS_Linux7.8.2003-x86_64.tgz
+cd qctool_v2.2.0-CentOS Linux7.8.2003-x86_64 || exit
+cp qctool /usr/bin/
