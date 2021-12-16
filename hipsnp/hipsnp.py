b3e0dd4d118a4a8fcc780d965134057419418469
oportoles <o.portoles@fz-juelich.de>
2021-12-16 13:12:13 +0100

    test for allele coputation

--- hipsnp/hipsnp.py
+++ hipsnp/hipsnp.py
@@ -832,7 +832,6 @@ def _find_chromosome_data(chr, datadir, subdir='imputation',
     files = list((datadir / subdir).glob(f'*_c{chr}_*'))
     got_files = []
     if datalad_source:
-        logger.info(f'Getting files: {files}')
         get_status = ds.get(files)  # type: ignore
         for f_val in get_status:
             status = f_val['status']
@@ -919,7 +918,6 @@ def genotype_from_datalad(
             warn(
                 f'Chromosome {t_chr} output file exists. '
                 'Skipping (set recompute=True to force generation)')
-            files_to_read.append(bgen_out)
             continue
 
         # get the data
